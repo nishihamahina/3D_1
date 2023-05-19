840c72ea6f264e796943f27ca8115c0e9e593dfe
nisihama_hina <zrhy20041025hian@icloud.com>
2023-05-19 11:35:03 +0900

    途中3

---
+++ Assets/sprkt/play_j_s.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class play_j_s : MonoBehaviour
+{
+    private bool jumpNow;
+    public float jumpPower;
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
