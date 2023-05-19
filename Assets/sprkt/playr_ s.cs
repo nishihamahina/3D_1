ff4de2791683db3ace56677ce100483c6e38d5d0
nisihama_hina <zrhy20041025hian@icloud.com>
2023-05-19 11:14:08 +0900

    途中1

---
+++ Assets/sprkt/playr_ s.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playr_s : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        var movement = new Vector3(Horizontal, 0, Vertical);
+        
+        rb.AddForce(movement * 3.0f);
+    }
+}
