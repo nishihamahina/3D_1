34d9c99197dffecc6a8a619be2eb17d5920ca571
nisihama_hina <zrhy20041025hian@icloud.com>
2023-05-19 12:29:46 +0900

    追尾機能追加

---
+++ Assets/sprkt/teki_s.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teki_s : MonoBehaviour
+{
+    [SerializeField]private GameObject player;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(player.transform.position - transform.position);
+    }
+}
